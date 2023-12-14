390feffbda380a1f782ce6c89d89be6edc9b33ae
senina mariya <senina.m.m@yandex.ru>
2023-12-14 11:22:14 +0300

    add formater

--- src/euler2.lisp
+++ src/euler2.lisp
@@ -1,18 +1,17 @@
-; в лоб через циклы
+					; в лоб через циклы
 (defun solve2_array ()
-    (loop for a = 1 then b and b = 1 then (+ a b) 
-        while (< a 4000000) 
-        when (evenp a) sum a
-    )
-)
+  (loop for a = 1 then b and b = 1 then (+ a b)
+	while (< a 4000000)
+	when (evenp a) sum a))
 
-; рекурсия
+					; рекурсия
 (defun solve2_recursion (a b)
   (if (< b 4000000) (+ (solve2_recursion b (+ a b)) (if (evenp b) b 0)) 0))
 
 (defun solve2_map ()
-    (reduce #'+ (mapcar #'(lambda(x) (if (evenp x) x 0)) (loop for a = 1 then b and b = 1 then (+ a b)
-                        while (< a 4000000)
-                        collect a))
-    )
-)
+  (reduce #'+
+	  (mapcar #'
+           (lambda(x) (if (evenp x) x 0))
+           (loop for a = 1 then b and b = 1 then (+ a b)
+		 while (< a 4000000)
+		 collect a ))))
